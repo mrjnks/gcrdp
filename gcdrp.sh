2a29ed7885d7ef7a185758fcfd179aaab720e26a
mrjnks <76967262+mrjnks@users.noreply.github.com>
2021-01-23 15:44:45 -0800

    Create gcdrp.sh

---
+++ gcdrp.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+printf "Installing RDP Be Patience... " >&2
+{
+ sudo -s <<HERE
+ read -p "Choose username: " usrn
+ read -p "Choose password: " mypswd
+ useradd -m $usrn
+ adduser $usrn sudo
+ printf '%s:%s' $usrn $mypswd | sudo chpasswd
+ sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+ apt update
+ sudo apt-get install --assume-yes wget
+ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+ dpkg --install chrome-remote-desktop_current_amd64.deb
+ apt install --assume-yes --fix-broken
+ DEBIAN_FRONTEND=noninteractive \
+ apt install --assume-yes xfce4 desktop-base
+ bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+ apt install --assume-yes xscreensaver
+ systemctl disable lightdm.service
+ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+ dpkg --install google-chrome-stable_current_amd64.deb
+ apt install --assume-yes --fix-broken
+ apt install nautilus nano -y 
+ adduser $usrn chrome-remote-desktop
+ } &> /dev/null &&
+ printf "\nSetup Complete " >&2 ||
+ printf "\nError Occured " >&2
+ printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+ read -p "Paste Here: " CRP
+ su - ALOK -c """$CRP"""
+ printf 'Check https://remotedesktop.google.com/access/ \n\n'
+ ipaddrs=$(curl ipecho.net/plain)
+ if sudo apt-get upgrade &> /dev/null
+ success="printf \n\n"All set. For reference, your IP is: $ipaddrs\"" >&2
+ then
+     /bin/sh -c "$success"
+ else
+     printf "\n\nError Occured " >&2
+ fi
+ HERE
